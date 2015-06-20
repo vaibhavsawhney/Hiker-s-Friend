cc279bc3e5a781134cf49f762fa8a56d4de3ddae
Saksham Saxena <saksham_saxena@outlook.com>
2015-06-20 17:21:54 +0530

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+/* Application Sripts */
+
+$(document).ready(function(){
+  $("#loginBox, footer").hide();
+  $("body").css("height",0.8832*window.screen.availHeight+"px");
+});
+
+//Show login box
+$(".starter-template button").click(function(){
+  $(".starter-template p, .starter-template button").fadeOut();
+  $(".starter-template h1").animate({fontSize:"4em", marginBottom:0},400);
+  $("#loginBox").delay(400).fadeIn();
+});
+
+//Show etc
+$(".glyphicon-th-large").click(function(){
+  $("footer").toggle("slow");
+})
